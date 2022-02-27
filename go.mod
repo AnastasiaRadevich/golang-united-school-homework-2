bc7528da8ff127993daf076c82034dab980f7e41
ARadzevich <Anastasiya_Radzevich@epam.com>
2022-02-27 13:51:10 +0300

    Revert "feat: add calcSquare function"
    This reverts commit cde818754a581d4e3b991ff83a1526e559b5fb59.

--- go.mod
+++
@@ -1,3 +0,0 @@
-module golang-united-school-homework-2/calc-square
-
-go 1.17
--- solution.go
+++ solution.go
@@ -1,21 +1,12 @@
 package square
 
-import (
-	"math"
-)
+// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
-type NumberShapeSides int64
+// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+// it's like:
+// CalcSquare(10.0, SidesTriangle)
+// CalcSquare(10.0, SidesSquare)
+// CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum NumberShapeSides) float64 {
-	const pi = 3.14
-	var result float64
-
-	if sidesNum == 3 {
-		result = math.Sqrt(3) / 4 * math.Pow(sideLen, 2)
-	} else if sidesNum == 4 {
-		result = math.Pow(sideLen, 2)
-	} else {
-		result = pi * math.Pow(sideLen, 2)
-	}
-	return result
+func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
 }
